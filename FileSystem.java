e23fd78f95ea40c9c53eb66b93e4c4da89efc187
SamaraCardoso27 <samaracardosodossantos@gmail.com>
2014-06-19 15:53:41 -0300

    sistema de arquivos a ser implementado

---
+++ FileSystem.java
@@ -0,0 +1,13 @@
+interface FileSystem {
+
+    public int formatDisk(int size, int iSize);   
+    public int shutdown();     
+    public int create();    
+    public int inumber(int fd);       
+    public int open(int iNumber);        
+    public int read(int fd, byte[] buffer); 
+    public int write(int fd, byte[] buffer); 
+    public int seek(int fd, int offset, int whence);     
+    public int close(int fd); 
+    public int delete(int iNumber); 
+}
